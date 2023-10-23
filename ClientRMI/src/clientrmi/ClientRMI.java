f19af5510464edb5ff48c1a6c5f745e034f11032
ismail <sorhraniismail@gmail.com>
2023-10-23 22:38:42 +0100

    update

---
+++ ClientRMI/src/clientrmi/ClientRMI.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientrmi;
+
+import dao.IDao;
+import entities.Machine;
+import entities.Salle;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ismail
+ */
+public class ClientRMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ ServeurRMI/src/test/Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao.IDao;
+import entities.Machine;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.Hibernate;
+import services.MachineService;
+import util.HibernateUtil;
+
+/**
+ *
+ * @author Ismail
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
